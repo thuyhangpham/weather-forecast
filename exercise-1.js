422e3ad81359213ae28a2576ff1afd5e863e2041
thuyhangpham <phamthuyhang214@gmail.com>
2026-04-04 03:05:40 +0700

    today's weather

---
+++ exercise-1.js
@@ -0,0 +1,8 @@
+let temperature = 22;
+let condition = 'rainy';
+let sunriseTime = '5:30 AM';
+let sunsetTime = '6:30 PM';
+
+console.log(`I like the weather today. 
+It is ${condition} and the temperature is ${temperature} degrees Celsius. 
+The sun will rise at ${sunriseTime} and set at ${sunsetTime}.`);   
